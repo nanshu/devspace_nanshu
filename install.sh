3d2fcb520a85c0faf6aecb0165d4bdab4d893434
Nanshu Chen <nanshu@gmail.com>
2025-10-27 16:23:21 +0000

    update

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo sh -c '
+add-apt-repository -y ppa:jgmath2000/et
+apt update -qq
+apt install -y et fzf build-essential libssl-dev ruby-dev 
+snap install nvim --classic
+snap install kubectx --classic
+gem install consul-templaterb
+gem install multitrap
+'
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install derailed/k9s/k9s
