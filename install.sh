336365b65ce4de5d5f0326ee0db8ca91d8c5448b
Nanshu Chen <nanshu@gmail.com>
2025-11-11 19:19:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,12 @@ gem install multitrap
 '
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install derailed/k9s/k9s jid pipx visidata bat eza pv btop glow ipcalc tldr
+brew install derailed/k9s/k9s jid pipx visidata bat eza pv btop glow ipcalc tldr atuin
 
 curl -L -o /tmp/sapling.deb https://github.com/facebook/sapling/releases/download/0.2.20250521-115337%2B25ed6ac4/sapling_0.2.20250521-115337%2B25ed6ac4_amd64.Ubuntu22.04.deb
 sudo dpkg -i /tmp/sapling.deb
+
+
 
 
 
